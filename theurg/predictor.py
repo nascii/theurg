3df1a53efcc65bed414b55a140870ad2a3592237
Paul Loyd <pavelko95@gmail.com>
2016-08-10 16:17:31 +0300

    chore(predictor): move out `HEROES`

--- theurg/predictor.py
+++ theurg/predictor.py
@@ -5,16 +5,10 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 
-from entities import Match
+from entities import Match, HEROES
 
-# TODO(loyd): move somewhere.
-HEROS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,25,26,27,28,29,30,31,32,33,34,
-         35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,
-         65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,
-         95,96,97,98,99,100,101,102,103,104,105,106,107,109,110,111,112,113]
-
-HERO_ID_MAP = {id: idx for idx, id in enumerate(HEROS)}
-HERO_COUNT = len(HEROS)
+HERO_ID_MAP = {id: idx for idx, id in enumerate(HEROES)}
+HERO_COUNT = len(HEROES)
 
 class Predictor:
     def __init__(self, db_path):
