594d92559801c5c6e6903072c4bc5344dc5c5880
Paul Loyd <pavelko95@gmail.com>
2016-08-10 16:02:17 +0300

    feat(aggregator): download ancient matches by heroes

--- theurg/aggregator.py
+++ theurg/aggregator.py
@@ -3,8 +3,8 @@ import time
 import logging
 import sqlite3
 
-from entities import Match, LeagueStats
-from fetcher import SteamAPI
+from entities import Match, LeagueStats, HEROES
+from fetcher import SteamAPI, Dotabuff
 
 _TYPE_MAP = {
     'int': 'INTEGER',
@@ -42,8 +42,10 @@ logger = logging.getLogger(__name__)
 
 class Aggregator:
     def __init__(self, db_path, api_key, retry_limit=None, retry_delay=None):
-        self.api = SteamAPI(api_key, retry_limit, retry_delay)
-        self.db = sqlite3.connect(db_path)
+        self._steam_api = SteamAPI(api_key, retry_limit, retry_delay)
+        self._dotabuff = Dotabuff()
+        self._db = sqlite3.connect(db_path)
+        self._match_filter = set()
 
         self._prepare_db()
 
@@ -71,24 +73,26 @@ class Aggregator:
             league_stats = self._download_ancient(league_stats)
 
     def _prepare_db(self):
-        self.db.row_factory = sqlite3.Row
+        self._db.row_factory = sqlite3.Row
 
-        with self.db:
-            self.db.execute('CREATE TABLE IF NOT EXISTS ' + _MATCH_DESCRIPTION)
-            self.db.execute('CREATE TABLE IF NOT EXISTS ' + _LEAGUE_STATS_DESCRIPTION)
+        with self._db:
+            self._db.execute('CREATE TABLE IF NOT EXISTS ' + _MATCH_DESCRIPTION)
+            self._db.execute('CREATE TABLE IF NOT EXISTS ' + _LEAGUE_STATS_DESCRIPTION)
 
     def _bootstrap(self, league_id):
         logger.info('Bootstrapping league #%d...', league_id)
 
-        matches, complete = self._download_by_history(league_id)
+        matches, complete, (min_match_id, max_match_id) = self._download_by_history(league_id)
 
         if not matches:
             logger.info('There are no available matches for league #%d', league_id)
+
+        if not min_match_id:
             return None
 
         league_stats = LeagueStats(league_id=league_id,
-                                   min_match_id=matches[0].match_id,
-                                   max_match_id=matches[-1].match_id,
+                                   min_match_id=min_match_id,
+                                   max_match_id=max_match_id,
                                    complete=complete)
 
         self._save_matches_and_league_stats(matches, league_stats)
@@ -101,26 +105,24 @@ class Aggregator:
 
         logger.info('Downloading recent matches for league #%d...', league_id)
 
-        matches, complete = self._download_by_history(league_id, max_saved_match_id)
+        matches, complete, (mid_match_id, max_match_id) = self._download_by_history(
+            league_id, min_match_id=max_saved_match_id)
 
         if not matches:
             logger.info('There are no new non-indexed matches for league #%d', league_id)
+
+        if not mid_match_id:
             return league_stats
 
-        max_match_id = matches[-1].match_id
-        mid_match_id = matches[0].match_id
-
         while not complete:
-            logger.info(
-                'Last saved match for league #%d is not reached. Downloading next chunk...',
-                league_id)
-            chunk, complete = self._download_by_history(league_id, max_saved_match_id, mid_match_id)
+            logger.info('Last saved match for league #%d is not reached', league_id)
 
-            if chunk:
-                matches.extend(chunk)
-                mid_match_id = chunk[0].match_id
+            chunk, complete, (mid_match_id, _) = self._download_by_history(
+                league_id, min_match_id=max_saved_match_id, max_match_id=mid_match_id)
 
-        league_stats = league_stats._replace(max_match_id = max_match_id)
+            matches.extend(chunk)
+
+        league_stats = league_stats._replace(max_match_id=max_match_id)
         self._save_matches_and_league_stats(matches, league_stats)
 
         return league_stats
@@ -129,21 +131,40 @@ class Aggregator:
         league_id = league_stats.league_id
         logger.info('Downloading ancient matches for league #%d...', league_id)
 
-        complete = False
+        max_of_min_id = 0
+        min_completed_id = league_stats.min_match_id
+        next_heroes = [(hero_id, min_completed_id) for hero_id in HEROES]
 
-        while not complete:
-            matches, complete = self._download_by_history(league_id,
-                                                          max_match_id=league_stats.min_match_id)
+        while next_heroes:
+            heroes, next_heroes = next_heroes, []
+            matches = []
 
-            min_match_id = matches[0].match_id if matches else league_stats.min_match_id
-            league_stats = league_stats._replace(min_match_id = min_match_id, complete = complete)
+            for hero_id, min_match_id in heroes:
+                chunk, complete, (min_match_id, _) = self._download_by_history(
+                    league_id, hero_id, max_match_id=min_match_id)
 
+                if not complete:
+                    next_heroes.append((hero_id, min_match_id))
+
+                    if min_match_id:
+                        max_of_min_id = max(min_match_id, max_of_min_id)
+                else:
+                    if min_match_id:
+                        min_completed_id = min(min_match_id, min_completed_id)
+
+                matches.extend(chunk)
+
+            min_match_id = max_of_min_id if next_heroes else min_completed_id
+            league_stats = league_stats._replace(min_match_id=min_match_id,
+                                                 complete=not next_heroes)
             self._save_matches_and_league_stats(matches, league_stats)
 
         return league_stats
 
-    def _download_by_history(self, league_id, min_match_id=None, max_match_id=None):
+    def _download_by_history(self, league_id, hero_id=None, min_match_id=None, max_match_id=None):
         suffix = ''
+        if hero_id:
+            suffix += ' and hero #{}'.format(hero_id)
         if min_match_id:
             suffix += ' (>#{})'.format(min_match_id)
         if max_match_id:
@@ -151,32 +172,47 @@ class Aggregator:
 
         logger.debug('Looking for matches for league #%d%s...', league_id, suffix)
 
-        history = self.api.match_history(league_id, max_match_id - 1 if max_match_id else None)
+        history = self._steam_api.match_history(league_id, hero_id,
+                                                max_match_id - 1 if max_match_id else None)
 
         complete = history['results_remaining'] == 0
         if not complete and min_match_id:
             complete = min_match_id in (m['match_id'] for m in history['matches'])
 
+        if not history['matches']:
+            return [], complete, (None, None)
+
+        # Filter the list of matches to download.
         low, high = min_match_id or 0, max_match_id or sys.maxsize
-        filtered = (m for m in history['matches'] if low < m['match_id'] < high)
-        relevant = sorted(filtered, key=lambda m: m['match_id'])
+        relevant = [m for m in history['matches'] if low < m['match_id'] < high]
+
+        min_match_id = min(m['match_id'] for m in history['matches'])
+        max_match_id = max(m['match_id'] for m in history['matches'])
+
+        unknown = (m for m in relevant if m['match_id'] not in self._match_filter)
+        ordered = sorted(unknown, key=lambda m: m['match_id'])
 
         matches = []
 
-        for meta in relevant:
+        # Download info about the matches.
+        for meta in ordered:
             match_id = meta['match_id']
             logger.debug('Downloading match #%d...', match_id)
 
-            json = self.api.match_details(match_id)
+            json = self._steam_api.match_details(match_id)
             json['series_id'] = meta.get('series_id')
 
             match = Match.from_json(json)
             matches.append(match)
 
-        return matches, complete
+        # Remember the matches to prevent re-downloading.
+        for match in matches:
+            self._match_filter.add(match.match_id)
+
+        return matches, complete, (min_match_id, max_match_id)
 
     def _load_league_stats(self, league_id):
-        cursor = self.db.execute('SELECT * FROM league_stats WHERE league_id = ?', (league_id,))
+        cursor = self._db.execute('SELECT * FROM league_stats WHERE league_id = ?', (league_id,))
         row = cursor.fetchone()
 
         return LeagueStats.from_row(row) if row else None
@@ -184,9 +220,9 @@ class Aggregator:
     def _save_matches_and_league_stats(self, matches, league_stats):
         rows = (match.to_row() for match in matches)
 
-        with self.db:
-            self.db.execute(_LEAGUE_STATS_INSERT_STMT, league_stats.to_row())
-            self.db.executemany(_MATCH_INSERT_STMT, rows)
+        with self._db:
+            self._db.execute(_LEAGUE_STATS_INSERT_STMT, league_stats.to_row())
+            self._db.executemany(_MATCH_INSERT_STMT, rows)
 
         logger.info('%d matches for league #%d have been added to the database',
                     len(matches), league_stats.league_id)
