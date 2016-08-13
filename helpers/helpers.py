9bada8608edbe121b70ad3c03f43098eee5a978c
Ivan Skorokhodov <iskorokhodov@gmail.com>
2016-08-13 15:51:36 +0300

    refactor(helpers): move helpers to special folder

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,7 +1,7 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), '../theurg'))
-
 import sqlite3
+import pandas as pd
+sys.path.append(os.path.join(os.path.dirname(__file__), '../theurg'))
 
 import config
 from entities import Match, HEROES
@@ -11,6 +11,14 @@ def load_matches():
     conn.row_factory = sqlite3.Row
     matches = [Match.from_row(row) for row in conn.execute('SELECT * FROM match')]
     conn.close()
+    return matches
+
+def load_matches_df():
+    connection = sqlite3.connect(config.get_path('aggregation', 'db-path'))
+    query = connection.execute('SELECT * FROM match')
+    columns = [column[0] for column in query.description]
+    matches = pd.DataFrame.from_records(data = query.fetchall(), columns = columns)
+
     return matches
 
 HERO_ID_MAP = {id: idx for idx, id in enumerate(HEROES)}
